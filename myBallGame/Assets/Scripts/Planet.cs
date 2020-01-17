7e2d994e8839de7798f7951aea2c8e33078abc2e
Nikiforov Dmitriy <212532@mail.ru>
2020-01-17 13:31:35 +0600

    Add IClickable interface and Planet ScriptableObject

---
+++ myBallGame/Assets/Scripts/Planet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Planet", menuName = "Planet")]
+public class NewBehaviourScript : ScriptableObject
+{
+    public float gravity;
+    public Color skyColor;
+}
